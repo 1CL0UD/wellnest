5670a2d2034505d3da0536b615d42495c3e40da8
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2024-08-11 01:05:44 +0700

    homescreen done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import PatientForm from '@/components/PatientForm';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-lg">
           <Image
@@ -14,7 +17,23 @@ export default function Home() {
             className="mb-12 h-10 w-fit"
           />
         </div>
+        <PatientForm />
+        <div className="text-14-regular mt-20">
+          <p className="justify-items-end text-dark-600 xl:text-left">
+            © 2024 CarePulse
+          </p>
+          <Link href={'?/admin=true'} className="text-green-500">
+            Admin
+          </Link>
+        </div>
       </section>
+      <Image
+        src={'/assets/images/onboarding-img.png'}
+        alt={'Onboarding'}
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%]"
+      />
     </main>
   );
 }
